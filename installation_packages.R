3ec8d01125648435b212649f04ae8651709e7698
ridsonap <alfrzlp@gmail.com>
2022-06-25 13:43:51 +0700

    tambah file

--- installation_packages.R
+++ installation_packages.R
@@ -1,8 +1,13 @@
 install.packages('pak')
 
+# Mencadangkan ------------------------------------------------------------
+all_pkg <- list.files(.libPaths()[1])
+all_pkg <- all_pkg[all_pkg != '_cache']
+write.table(all_pkg, 'all_pkg.txt', sep = '', row.names = F, quote = F)
 
-# -------------------------------------------------------------------------
-all_pkg <- list.files('F:/4.2')
+
+# Install -----------------------------------------------------------------
+ll_pkg <- read.table('all_pkg.txt', header = T)$x
 
 sapply(
   all_pkg,
@@ -13,8 +18,6 @@ sapply(
     )
   }
 )
-
-
 
 # github ------------------------------------------------------------------
 devtools::install_github('alfrzlp/package-dm')
